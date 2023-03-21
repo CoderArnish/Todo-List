fe4cd5367376978692b7e803b53badd87cf0977f
CoderArnish <aa6908@srmist.edu.in>
2023-03-21 15:31:45 +0530

    Navbar Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='h-screen bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 overflow-x-hidden'>  
+        <Navbar/>
+        <div className='flex h-10 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-20'>
+          <div className='my-20 sm:mt-10 mx-auto space-y-3 bg-white'>
+            <Form/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='bg-white'>
+    <nav id='nav' className='h-[60px] bg-opacity-75 flex justify-between items-center w-[92%] mx-auto'>
+      <div id='logo' className='font-bold text-[#9333EA] font-roboto text-2xl'>
+        TaskMate
+      </div>
+      <div id='list'>
+        <ul className='flex items-center gap-[5vw]'>
+          <li><a className='hover:text-gray-500 text-xl' href="#">Todo</a></li>
+          <li><a className='hover:text-gray-500 text-xl' href="#">Project</a></li>
+          <li><a className='hover:text-gray-500 text-xl' href="#">Team</a></li>
+        </ul>
+      </div>
+      <div className='sign-up-logo'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+        </svg>
+      </div>
+    </nav>
+    </div>
+  )
+}
