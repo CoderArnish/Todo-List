89419abe496c9838640e7d88fa55d5fdecd8e0ea
CoderArnish <aa6908@srmist.edu.in>
2023-03-22 19:53:49 +0530

    Added functionality to all components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,56 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
+import Todo from "./components/Todo";
+import { useState,useEffect } from "react";
 
 
 
 function App() {
+  let initTodo;
+  if(localStorage.getItem("todos")==null){
+    initTodo=[];
+  }
+  else{
+    initTodo= JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const deleteTask=(todo)=>{
+    setTodos(todos.filter((e)=>{
+      return e !== todo;
+    }));
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }
+  
+  const addTask = (task)=>{
+    let sno;
+    if(todos.length===0){
+      sno=0;
+    }
+    else{
+      sno= todos[todos.length-1].sno+1;
+    }
+    const myTodo={
+      sno:sno,
+      task:task,
+    }
+    setTodos([...todos,myTodo]);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }, [todos])
+
   return (
     <div className="App">
       <div className='h-screen bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 overflow-x-hidden'>  
-        <Navbar/>
+        <Navbar/> 
         <div className='flex h-10 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-20'>
           <div className='my-20 sm:mt-10 mx-auto space-y-3 bg-white'>
-            <Form/>
+            <Form addTask={addTask}/>
+            <Todo todos={todos} deleteTask={deleteTask}/>
           </div>
         </div>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,35 @@
 import React from 'react'
+import { useState } from "react";
 
-export default function Form({addTask}) {
+const Form = ({addTask}) => {
+  const [task, setTask] = useState("");
+  const submit=(e)=>{
+    e.preventDefault();
+    if(!task)
+    {
+      alert("Task cannot be blank");
+    }
+    else{
+      addTask(task);
+      setTask("");
+    }
+  }
+
   return (
     <div className='mr-3 mt-2 container flex justify-center items-center'>
-      <form>
+      <form onSubmit={submit}>
         <div className='relative flex items-center text-gray-400 focus-within:text-gray-600'>
-          <input type="text" placeholder='Add Task' className='bg-white h-16 w-96 rounded-3xl relative px-8 py-2 text-xl ring-2 ring-gray-400 focus:ring-2 focus:ring-gray-500' />
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-plus-circle absolute ml-80 pointer-events-none" viewBox="0 0 16 16">
+          <input type="text" placeholder='Add Task' value={task} onChange={(e)=>{setTask(e.target.value);}} className='bg-white h-16 w-96 rounded-3xl relative px-8 py-2 text-xl ring-2 ring-gray-400 focus:ring-2 focus:ring-gray-500' />
+            <button type="submit" className='absolute ml-80'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
               <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
             </svg>
+            </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
+
+export default Form;
